39d272e7972be7a1efb5ee0e49cd0b4e4cd72c7b
Matt S Trout <matthewt@hercule.scsys.co.uk>
2010-11-16 03:25:50 +0000

    Make perl -Moo DTRT

--- lib/oo.pm
+++ lib/oo.pm
@@ -3,9 +3,24 @@ package oo;
 use strictures 1;
 use Moo::_Utils;
 
+sub moo {
+  print <<'EOMOO';
+ ______
+< Moo! >
+ ------
+        \   ^__^
+         \  (oo)\_______
+            (__)\       )\/\
+                ||----w |
+                ||     ||
+EOMOO
+  exit 0;
+}
+
 BEGIN {
     my $package;
     sub import {
+        moo() if $0 eq '-';
         $package = $_[1] || 'Class';
         if ($package =~ /^\+/) {
             $package =~ s/^\+//;
