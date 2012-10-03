7afe3b3ecf02a353b958001da88b40fa38ffcd7c
Matt S Trout <mst@shadowcat.co.uk>
2012-10-03 16:50:39 +0000

    bump version

--- lib/Moo.pm
+++ lib/Moo.pm
@@ -5,7 +5,7 @@ use Moo::_Utils;
 use B 'perlstring';
 use Sub::Defer ();
 
-our $VERSION = '1.000003'; # 1.0.3
+our $VERSION = '1.000004'; # 1.0.4
 $VERSION = eval $VERSION;
 
 require Moo::sification;
