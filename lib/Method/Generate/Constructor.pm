ce6724361412efcecaf73f16a2d65564dfe52d0e
Graham Knop <haarg@haarg.org>
2014-05-09 12:02:34 -0400

    don't depend on Moo::Object saving all constructor arguments

--- lib/Method/Generate/Constructor.pm
+++ lib/Method/Generate/Constructor.pm
@@ -2,7 +2,6 @@ package Method::Generate::Constructor;
 
 use strictures 1;
 use Sub::Quote qw(quote_sub unquote_sub quotify);
-use base qw(Moo::Object);
 use Sub::Defer;
 use Moo::_Utils qw(_getstash);
 
@@ -201,7 +200,13 @@ sub _check_required {
 }
 
 use Moo;
-Moo->_constructor_maker_for(__PACKAGE__)->register_attribute_specs(
+# bootstrap our own constructor
+sub new {
+  my $class = shift;
+  bless $class->BUILDARGS(@_), $class;
+}
+Moo->_constructor_maker_for(__PACKAGE__)
+->register_attribute_specs(
   attribute_specs => {
     is => 'ro',
     reader => 'all_attribute_specs',
