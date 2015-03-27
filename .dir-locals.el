acd32061fbb14690192c18c5288ae0a1f73eada7
Dagfinn Ilmari Mannsåker <ilmari@ilmari.org>
2015-03-27 13:58:10 +0100

    Add basic .dir-locals.el file for Emacs

---
+++ .dir-locals.el
@@ -0,0 +1,8 @@
+((nil . ((indent-tabs-mode . nil)
+         (require-final-newline . t)))
+ (cperl-mode . ((cperl-indent-level . 2)
+                (cperl-continued-statement-offset . 2)
+                (cperl-continued-brace-offset . 0)
+                (cperl-close-paren-offset . -2)
+                (cperl-indent-subs-specially . nil)
+                (indent-tabs-mode . nil))))
